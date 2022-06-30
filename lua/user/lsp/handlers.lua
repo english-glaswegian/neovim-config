766cec9fba6089e9f7135c08a5adf090ad11a1a1
John Pitts <irvinenomore@protonmail.com>
2022-06-30 11:55:55 -0400

    Modified handlers.lua to link to icons.lua

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -10,12 +10,13 @@ M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 
 M.setup = function()
+  local icons = require "user.icons"
   local signs = {
 
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
+    { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+    { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+    { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+    { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
   }
 
   for _, sign in ipairs(signs) do
